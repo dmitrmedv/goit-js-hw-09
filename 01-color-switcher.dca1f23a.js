8c8513fe4a4f558a962006e41dcc9e3ffb84116c
dmitrmedv <106028486+dmitrmedv@users.noreply.github.com>
2023-07-21 12:12:55 +0000

    Deploying to gh-pages from @ dmitrmedv/goit-js-hw-09@28fb58b9bc16ffbfadc36683bbfb368e840539cb 🚀

--- 01-color-switcher.dca1f23a.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");t.addEventListener("click",(function(){t.disabled=!0,o=setInterval((function(){console.log(r()),n.style.backgroundColor=r()}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,clearInterval(o)}));var o=null;function r(){return"#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}}();
-//# sourceMappingURL=01-color-switcher.dca1f23a.js.map
